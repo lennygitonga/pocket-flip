5064b7139c576826a8533ec4bc3fc7ef66896857
Lenny Gitonga <lennygitonga07@gmail.com>
2026-03-15 11:54:12 +0300

    add components

---
+++ pocket-flip/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Search-bar from './components/Search-bar'
+import Book-list from './components/Book-list'
+import './index.css'
+
+function App() {
+
+}
+
+export default App
